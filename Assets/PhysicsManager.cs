7802506c168901216436083967a20be1b9ef4fea
unknown <ks6332@IGMGRAD-06.main.ad.rit.edu>
2015-09-08 19:32:08 -0400

    Physics Manager Update

--- Assets/PhysicsManager.cs
+++ Assets/PhysicsManager.cs
@@ -5,23 +5,41 @@ public class PhysicsManager : MonoBehaviour
 {
     [SerializeField]
     private float changeValue;
+    private float minMass;
+    private float maxMass;
+    private float currentMass;
+    private PhysicMaterial playerPhyscMat;
+
 	void Start () {
-	
+	    currentMass = gameObject.GetComponent<Rigidbody>().mass;
+        playerPhyscMat = gameObject.GetComponent<BoxCollider>().material;
 	}
 	void Update () {
 	
 	}
 
     //access the mass from rigidbody
-    void changeMass()
+    void changeMass(float triggerValue)
     {
-        //if trigger pressed
-        gameObject.GetComponent<Rigidbody>().mass += changeValue;
+        float prvFrameTriggerValue = triggerValue;
 
-        //if trigger released
-        gameObject.GetComponent<Rigidbody>().mass -= changeValue;
+        //if trigger pressed
+        if(triggerValue >= prvFrameTriggerValue)
+        {
+            if(currentMass < maxMass)
+            {
+                currentMass += changeValue * triggerValue * Time.deltaTime;
+            }
+            else
+            {
+                currentMass -= changeValue * triggerValue * Time.deltaTime;
+            }
+        }
     }
     
     //access physics mat
-    void 
+    void changeBounciness(float triggerValue)
+    {
+        playerPhyscMat.bounciness *= triggerValue * Time.deltaTime;
+    }
 }

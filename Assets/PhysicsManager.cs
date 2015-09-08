e9bb1370700df63eaf15971a6cc90f4b9398bca0
unknown <ks6332@IGMGRAD-06.main.ad.rit.edu>
2015-09-08 19:03:15 -0400

    Physics manager

---
+++ Assets/PhysicsManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PhysicsManager : MonoBehaviour 
+{
+    [SerializeField]
+    private float changeValue;
+	void Start () {
+	
+	}
+	void Update () {
+	
+	}
+
+    //access the mass from rigidbody
+    void changeMass()
+    {
+        //if trigger pressed
+        gameObject.GetComponent<Rigidbody>().mass += changeValue;
+
+        //if trigger released
+        gameObject.GetComponent<Rigidbody>().mass -= changeValue;
+    }
+    
+    //access physics mat
+    void 
+}

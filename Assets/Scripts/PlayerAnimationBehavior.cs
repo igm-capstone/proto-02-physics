48051ac90a3eacf5012e90224a66f840e1e5375a
Gabriel Ortega <gabriel.ortega56@gmail.com>
2015-09-09 09:12:58 -0400

    Animation Script

---
+++ Assets/Scripts/PlayerAnimationBehavior.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerAnimationBehavior : MonoBehaviour 
+{
+    
+	// Use this for initialization
+	void Start () 
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	
+	}
+
+    public void Scale(float input)
+    {
+        float y = Mathf.Min(1.0f, 1.7f - input);
+        float x = Mathf.Max(0.1f + input, 1.0f);
+        float z = x;
+        transform.localScale = new Vector3(x, y, z);
+    }
+}

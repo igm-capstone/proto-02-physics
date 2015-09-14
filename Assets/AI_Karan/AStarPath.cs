6675919342879705d6e69221bf881ff68bba6290
Karan Sahu <karansahu1991@gmail.com>
2015-09-14 00:23:01 -0400

    AI path finding working with heap structure. Need to make the enemy follow the path.

---
+++ Assets/AI_Karan/AStarPath.cs
@@ -0,0 +1,112 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+public class AStarPath : MonoBehaviour 
+{
+    
+    //Node_K sourceNode, targetNode;
+    AIArea AIarea;
+    public LayerMask AILayer;
+    public Transform source, target;
+    private List<Node_K> path = new List<Node_K>();
+
+    void Start()
+    {
+        AIarea = GetComponent<AIArea>();    
+    }
+
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.Space))
+            aStar(source.position, target.position);
+    }
+
+    public void aStar(Vector3 startPos, Vector3 endPos)
+    {
+        Stopwatch watch = new Stopwatch();
+        watch.Start();
+
+        //List<Node_K> openSet = new List<Node_K>();
+        Heap<Node_K> openSet = new Heap<Node_K>(AIarea.GetMaxSize);
+        HashSet<Node_K> closedSet = new HashSet<Node_K>();
+        Node_K sourceNode = AIarea.getNodeAtPos(startPos);
+        Node_K targetNode = AIarea.getNodeAtPos(endPos);
+        openSet.Add(sourceNode);        
+
+        while(openSet.Count > 0)
+        {
+            Node_K currentNode = openSet.RemoveFirst();
+            //Node_K currentNode = openSet[0];
+            //for (int i = 1; i < openSet.Count; i++)
+            //{
+            //    if (openSet[i].fCost < currentNode.fCost || openSet[i].fCost == currentNode.fCost && openSet[i].hCost < currentNode.hCost)
+            //    {
+            //        currentNode = openSet[i];
+            //    }
+            //}
+
+            //openSet.Remove(currentNode);
+            closedSet.Add(currentNode);
+
+            if(currentNode == targetNode)
+            {
+                reversePath(sourceNode, targetNode);
+                watch.Stop();
+                print(watch.ElapsedMilliseconds + "ms");
+                return;
+            }
+
+            List<Node_K> neighbors = new List<Node_K>();
+            neighbors = AIarea.neighbors(currentNode);
+            for (int i = 0; i < neighbors.Count; i++)
+            {
+                if (neighbors[i].walkable == false || closedSet.Contains(neighbors[i]))
+                    continue;
+
+                int cost = currentNode.gCost + moveCost(currentNode, neighbors[i]);
+                if (!openSet.Contains(neighbors[i]) || (cost < neighbors[i].gCost))
+                {
+                    neighbors[i].gCost = cost;
+                    neighbors[i].hCost = moveCost(neighbors[i], targetNode);
+                    neighbors[i].parent = currentNode;
+
+                    if (!openSet.Contains(neighbors[i]))
+                        openSet.Add(neighbors[i]);
+                    else
+                        openSet.UpdateItem(neighbors[i]);
+                }
+            }
+        }
+    }
+
+    void reversePath(Node_K source, Node_K target)
+    {
+        List<Node_K> revPath = new List<Node_K>();
+        Node_K node = target;
+        while(node != source)
+        {
+            revPath.Add(node);
+            node = node.parent;
+        }
+        revPath.Reverse();
+
+        //for (int i = 0; i < revPath.Count; i++)
+        //{
+        //    print("path " + i + revPath[i].myPos);
+        //}
+        AIarea.path = revPath;
+    }
+
+    int moveCost(Node_K source, Node_K target)
+    {
+        float zCost = Mathf.Abs(target.myCoord.y - source.myCoord.y);
+        float xCost = Mathf.Abs(target.myCoord.x - source.myCoord.x);
+
+        if (xCost > zCost)
+            return Mathf.RoundToInt((14 * zCost) + 10*(xCost - zCost));
+        else
+            return Mathf.RoundToInt((14 * xCost) + 10 * (zCost - xCost));
+    }
+}
---
+++ Assets/AI_Karan/Heap.cs
@@ -0,0 +1,108 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Heap<T> where T : IHeapObj<T>
+{
+    T[] heapObjects;
+    int heapCount;
+
+    public Heap(int maxSize)
+    {
+        heapObjects = new T[maxSize];
+    }
+
+    public void Add(T obj)
+    {
+        obj.heapIndex = heapCount;
+        heapObjects[heapCount] = obj;
+        SortUp(obj);
+        heapCount++;
+    }
+
+    void SortUp(T obj)
+    {
+        int parentIndex = (obj.heapIndex - 1) / 2;
+        while(true)
+        {
+            T parentItem = heapObjects[parentIndex];
+            //cmpare and check with the parent if the priority is higher, if yes, swap. if not break. while its true, keep calculating the parent index
+            if (obj.CompareTo(parentItem) > 0)
+                Swap(obj, parentItem);
+            else
+                break;
+
+            parentIndex = (obj.heapIndex - 1) / 2;
+        }
+    }
+
+    void Swap(T objA, T objB)
+    {
+        heapObjects[objA.heapIndex] = objB;
+        heapObjects[objB.heapIndex] = objA;
+        int tempObjAIndex = objA.heapIndex;
+        objA.heapIndex = objB.heapIndex;
+        objB.heapIndex = tempObjAIndex;
+    }
+
+    public T RemoveFirst()
+    {
+        T firstObj = heapObjects[0];
+        heapCount--;
+        heapObjects[0] = heapObjects[heapCount];
+        heapObjects[0].heapIndex = 0;
+        SortDown(heapObjects[0]);
+        return firstObj;
+    }
+
+    void SortDown(T obj)
+    {
+        while(true)
+        {
+            int indexLeftChild = (2 * obj.heapIndex) + 1;
+            int indexRightChild = (2 * obj.heapIndex) + 2;
+            int indexToSwap = 0;
+        
+            if (indexLeftChild < heapCount)
+            {
+                indexToSwap = indexLeftChild;          
+                if (indexRightChild < heapCount)
+                {
+                    if (heapObjects[indexLeftChild].CompareTo(heapObjects[indexRightChild]) < 0)
+                        indexToSwap = indexRightChild;               
+                }
+                if (obj.CompareTo(heapObjects[indexToSwap]) < 0)
+                    Swap(obj, heapObjects[indexToSwap]);
+                else
+                    return;
+
+            }
+            else
+                return;
+        }
+    }
+
+    public int Count
+    {
+        get { return heapCount; }
+    }
+
+    public void UpdateItem(T obj)
+    {
+        SortUp(obj);
+    }
+
+    public bool Contains(T obj)
+    {
+        return Equals(heapObjects[obj.heapIndex], obj);
+    }
+}
+
+public interface IHeapObj<T> : IComparable<T>
+{
+    int heapIndex
+    {
+        get;
+        set;
+    }
+}
